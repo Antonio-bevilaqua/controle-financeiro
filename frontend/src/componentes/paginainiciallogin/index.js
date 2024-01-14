e000e80d895e24798d44854d129649e60e9355a3
Rodolpho Hora <rodolpho.hooraa@gmail.com>
2024-01-13 23:02:27 -0300

    Login-Page creation
    - Box with "name" and "password" fields added and styled.

--- frontend/src/componentes/paginainiciallogin/index.js
+++ frontend/src/componentes/paginainiciallogin/index.js
@@ -2,8 +2,39 @@ import './index.css';
 
 export default function Login() {
     return(
-        <div>
-            teste de Login
+        <div className="main">
+
+            <div className="container">
+
+                <div className="containerCardLogin">
+
+                    <h1 className="tituloLogin"> Logar </h1>
+
+                    <div className="campoNomeLogin">
+                        <label htmlFor=""> Nome </label>
+                        <input type="text" />
+                    </div>
+
+                    <div className="campoSenhaLogin">
+                        <label htmlFor=""> Senha </label>
+                        <input type="password" />
+                    </div>
+
+                    <div className="containerBotoesLogin">
+
+                        <button>
+                            <img src="../../assets/cadeado.svg" alt="" /> Log in
+                        </button>
+
+                        <button className="botaoSecundarioLogin"> Criar conta
+                        </button>
+
+                        <button className="botaoSecundarioLogin"> Esqueci a senha
+                        </button>
+                        
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
