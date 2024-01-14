a34b79d4e3e14922449848d2ca5dc5a28747a76a
Cozac <victorcozac32@gmail.com>
2024-01-14 12:19:22 -0300

    Update
    Update Layout
    End CSS Login Page
    Responsive LoginPage

--- frontend/src/componentes/paginainiciallogin/index.js
+++ frontend/src/componentes/paginainiciallogin/index.js
@@ -1,47 +1,35 @@
 import './index.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLock} from '@fortawesome/free-solid-svg-icons'
-import { faUser } from '@fortawesome/free-regular-svg-icons'
 
 export default function Login() {
     return(
-        <div className="main">
-
-            <div className="container">
-
-                <div className="containerCardLogin">
-
-                    <h1 className="tituloLogin"> Logar </h1>
-
-                    <div className="campoNomeLogin">
-                        <label htmlFor=""> <p>Nome</p> </label>
-                        <input type="text" />
+        <main className="container">
+            <div className="containerCardLogin">
+                <h1 className="tituloLogin"> Logar </h1>
+                <div className="containerLabelInputLogin">
+                    <label htmlFor="usuario">Usuario</label>
+                    <input type="text" name='usuario' id='usuario' />
+                </div>
+                <div className="containerLabelInputLogin">
+                    <label htmlFor="password">Senha</label>
+                    <input type="password" name='password' id='password' />
+                </div>
+                <div className="containerBotoesLogin">
+                    <div  className="botaoLogin" id='botaoPrimarioLogin'>
+                        <FontAwesomeIcon icon={faLock} />
+                        <p>Log in</p>
                     </div>
-
-                    <div className="campoSenhaLogin">
-                        <label htmlFor=""> <p>Senha</p> </label>
-                        <input type="password" />
-                    </div>
-
-                    <div className="containerBotoesLogin">
-
-                        <div  className="botaoPrimarioLogin">
-                            <FontAwesomeIcon icon={faLock} />
-                            <p>Log in</p>
-                        </div>
-
-                        <div className="botaoSecundarioLogin"> 
-                            <FontAwesomeIcon icon={faUser} />
+                    <div id='dimensionamentoBotao'>
+                        <div className="botaoLogin">
                             <p>Criar conta</p>
                         </div>
-
-                        <div className="botaoSecundarioLogin"> 
+                        <div className="botaoLogin">
                             <p>Esqueci a senha</p>
                         </div>
-                        
                     </div>
                 </div>
             </div>
-        </div>
+        </main>
     );
 }
