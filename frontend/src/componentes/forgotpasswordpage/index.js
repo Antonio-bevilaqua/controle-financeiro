f6b4b22954b1c6fcf6184cdbea617490ceac6ab3
Cozac <victorcozac32@gmail.com>
2024-01-17 20:11:44 -0300

    Update
    Delete Unnecessary JS and CSS

--- frontend/src/componentes/forgotpasswordpage/index.js
+++ frontend/src/componentes/forgotpasswordpage/index.js
@@ -1,36 +1,24 @@
 import './index.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLock, faSpinner } from '@fortawesome/free-solid-svg-icons';
-import { useContext, useState } from 'react';
-import { UserContext } from '../../contexts/UserContextProvider';
-import { Navigate, useNavigate } from 'react-router-dom';
 
 
 export default function ForgotPassword() {
-
-    const context = useContext(UserContext);
-    const [ User, setUser ] = useState("");
-    const [ Loading, setLoading ] = useState(false);
-    const Navigate = useNavigate();
-
-    const ValidUser = () => {
-        const regex = /^[ a-zA-Z]+$/;
-        const isValid = regex.test(User) && User.length >= 3 && User.length < 16;
-        return isValid;
-    }
-
-    const checkValidation = async () => {
-        setLoading(true);
-        await context.login(User);
-
-        setLoading(false);
-    }
-
-    var UserChecker = ValidUser() ? 'Valido' : 'Invalido' ;
-
     return (
-        <main>
-            
-        </main>
+    <div className="main-box">
+        <h1>recuperar senha</h1>
+        <form action="">
+            <div className="input-box">
+                <label>nova senha</label>
+                <input type="email" required />
+            </div>
+            <div className="input-box">
+                <label>confirmar senha</label>
+                <input type="password" required />
+            </div>
+            <div className="check">
+                <label>confirmar senha</label>
+                <input type="password"></input>
+            </div>
+        </form>
+    </div>
     );
-    }
+}
